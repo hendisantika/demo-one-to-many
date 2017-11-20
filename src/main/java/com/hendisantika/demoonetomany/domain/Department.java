b6285018dc6bb6f8cbba73237214ca4aaa3113cf
Hendi Santika <hendisantika@gmail.com>
2017-11-21 05:40:49 +0700

    #6 - Add Departement Domain

---
+++ src/main/java/com/hendisantika/demoonetomany/domain/Department.java
@@ -0,0 +1,74 @@
+package com.hendisantika.demoonetomany.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : demo-one-to-many
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/11/17
+ * Time: 05.39
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Entity
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    protected Date dateCreated;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    protected Date lastUpdated;
+
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "department", fetch = FetchType.LAZY)
+    private List<Employee> employees;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
