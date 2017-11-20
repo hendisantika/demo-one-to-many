5e56976d1765cdf73fdb61c0ceded80d657b5646
Hendi Santika <hendisantika@gmail.com>
2017-11-21 05:39:37 +0700

    #5 - Add Address Domain

---
+++ src/main/java/com/hendisantika/demoonetomany/domain/Address.java
@@ -0,0 +1,85 @@
+package com.hendisantika.demoonetomany.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : demo-one-to-many
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/11/17
+ * Time: 05.38
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    protected Date dateCreated;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    protected Date lastUpdated;
+
+    private String address;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Employee employee;
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", dateCreated=" + dateCreated +
+                ", lastUpdated=" + lastUpdated +
+                ", address='" + address + '\'' +
+                ", employee=" + employee +
+                '}';
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+}
