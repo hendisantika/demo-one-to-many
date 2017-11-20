fa386095a9773fd8f105a51d38f8a08808c00136
Hendi Santika <hendisantika@gmail.com>
2017-11-21 05:34:59 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/demoonetomany/DemoOneToManyApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.demoonetomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOneToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOneToManyApplication.class, args);
+	}
+}
