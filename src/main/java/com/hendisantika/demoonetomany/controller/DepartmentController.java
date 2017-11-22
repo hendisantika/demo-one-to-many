786d1122707a66ff2ac3cbefe2890b6d7286105c
Hendi Santika <hendisantika@gmail.com>
2017-11-23 04:49:09 +0700

    #31 - Modify Department Controller

--- src/main/java/com/hendisantika/demoonetomany/controller/DepartmentController.java
+++ src/main/java/com/hendisantika/demoonetomany/controller/DepartmentController.java
@@ -25,43 +25,44 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(path = "/department")
 public class DepartmentController {
     private static final String PATH = "/department";
+
     @Autowired
     private DepartmentService departmentService;
 
-    @RequestMapping(value = {"","/list"},method = RequestMethod.GET)
-    public String list(Model model){
+    @RequestMapping(value = {"", "/list"}, method = RequestMethod.GET)
+    public String list(Model model) {
         model.addAttribute("departments", departmentService.list());
-        return PATH+"/list";
+        return PATH + "/list";
     }
 
     @RequestMapping(value = "/show/{id}", method = RequestMethod.GET)
-    public String show(@PathVariable("id")long id, Model model){
-        model.addAttribute("department",departmentService.get(id));
-        return PATH+"/show";
+    public String show(@PathVariable("id") long id, Model model) {
+        model.addAttribute("department", departmentService.get(id));
+        return PATH + "/show";
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.GET)
-    public String create(Model model){
-        return PATH+"/create";
+    public String create(Model model) {
+        return PATH + "/create";
     }
 
     @RequestMapping(value = "save", method = RequestMethod.POST)
-    public String save(@ModelAttribute Department department, Model model){
+    public String save(@ModelAttribute Department department, Model model) {
         department = departmentService.save(department);
-        model.addAttribute("department",department);
-        return "redirect:"+PATH+"/show/"+department.getId();
+        model.addAttribute("department", department);
+        return "redirect:" + PATH + "/show/" + department.getId();
     }
 
     @RequestMapping(value = "/update", method = RequestMethod.PUT)
-    public String update(@ModelAttribute Department department,Model model){
+    public String update(@ModelAttribute Department department, Model model) {
         department = departmentService.update(department);
-        model.addAttribute("department",department);
-        return "redirect:"+PATH+"/show/"+department.getId();
+        model.addAttribute("department", department);
+        return "redirect:" + PATH + "/show/" + department.getId();
     }
 
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
-    public String delete(@PathVariable("id")long id,Model model){
-        model.addAttribute("department",departmentService.delete());
+    public String delete(@PathVariable("id") long id, Model model) {
+        model.addAttribute("department", departmentService.delete());
         return PATH;
     }
 
