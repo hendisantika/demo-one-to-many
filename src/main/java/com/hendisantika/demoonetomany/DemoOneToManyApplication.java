84c097e55f242cb3e6ceceef9a47d4f14a276e98
Hendi Santika <hendisantika@gmail.com>
2017-11-23 04:33:32 +0700

    #30 - Modify Main App

--- src/main/java/com/hendisantika/demoonetomany/DemoOneToManyApplication.java
+++ src/main/java/com/hendisantika/demoonetomany/DemoOneToManyApplication.java
@@ -1,15 +1,9 @@
 package com.hendisantika.demoonetomany;
 
-import com.hendisantika.demoonetomany.domain.Address;
-import com.hendisantika.demoonetomany.domain.Department;
-import com.hendisantika.demoonetomany.domain.Employee;
-import com.hendisantika.demoonetomany.repository.EmployeeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoOneToManyApplication{
@@ -20,13 +14,13 @@ public class DemoOneToManyApplication{
 		SpringApplication.run(DemoOneToManyApplication.class, args);
 	}
 
-    @Bean
-    public CommandLineRunner demo(EmployeeRepository repository) {
-        return (args) -> {
-            // save Employees
+//    @Bean
+//    public CommandLineRunner demo(EmployeeRepository repository) {
+//        return (args) -> {
+//            // save Employees
+////            repository.save(new Employee("hendisantika@konohagakure.com", "Hendi Santika", "hendi", new Address("Konohagakure"), new Department("Jounin")));
 //            repository.save(new Employee("hendisantika@konohagakure.com", "Hendi Santika", "hendi", new Address("Konohagakure"), new Department("Jounin")));
-            repository.save(new Employee("hendisantika@konohagakure.com", "Hendi Santika", "hendi", new Address("Konohagakure"), new Department("Jounin")));
-        };
-    }
+//        };
+//    }
 
 }
